78a88433fd8c0a73f3aaa4c0e980afcecafe7dde
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2020-03-23 12:49:04 +1300

    Update

---
+++ index.js
@@ -0,0 +1,44 @@
+/*
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+for (let i = 5; i > 0; i--) {
+    console.log(i);
+}
+
+for (let i = 0;; i++) {
+    console.log("loop, i = " + i);
+    if (i > 1) {
+        break;
+    }
+}
+
+let counter = 0;
+function inc() { counter++; } for (let i = 0; i < 10; i++, in());
+console.log(counter);
+
+let counter = 0;
+for (let i = 0; i < 10; i++) {
+    counter++
+}
+console.log(counter);
+
+for (var i = 0; i < 10; i++) let x = i;
+
+for (var i = 0; i < 10; i++) {let x = i; console.log(x);} // Must add curly brckets, otherwise will show an error
+
+for (let x = 0; x < 2; x++)         // 3 Dimensional Array
+    for (let y = 0; y < 2; y++)
+        for (let z = 0; z < 2; z++) {
+            console.log(x, y, z);
+        }
+
+for (let x = 0; x < 2; x++)         // 2 Dimensional Array
+    for (let y = 0; y < 2; y++) {
+        console.log(x, y);
+    }
+*/
+for (let i = 0; i < 3; i++) {
+  console.log("Loop.");
+}
