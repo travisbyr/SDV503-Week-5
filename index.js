0724eaf0ecd4f9c0806bc2468dd0219ce043d109
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2020-03-23 13:27:47 +1300

    Update

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ for (let i = 0;; i++) { // Breaking a loop
 
 let c = 0;
 mark: for (let i = 0; i < 5; i++)
-  inner: for (let j = 0; j < 5; j++) {
+  inner: for (let j = 0; j < 5; j++) { // break to a label
     c++;
     if (i == 2) {
       break mark;
